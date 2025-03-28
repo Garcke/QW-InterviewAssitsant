fbcee5d7c0d14b09bf882948e0ff4766f973e19c
Garcke <117699891+Garcke@users.noreply.github.com>
2025-03-28 17:53:01 +0800

    Add files via upload

---
+++ asrchat_config/APIkeyConfig.py
@@ -0,0 +1,10 @@
+# 请用百炼API Key将下行替换为：api_key="sk-xxx",网址：https://bailian.console.aliyun.com/?apiKey=1#/api-key
+DASHSCOPE_API_KEY = ''
+DEEPSEEK_API_KEY = ''
+
+# 将所有配置打包到一个字典中
+config = {
+    "DEEPSEEK_API_KEY": DEEPSEEK_API_KEY,
+    "DASHSCOPE_API_KEY":DASHSCOPE_API_KEY,
+    # 其他配置项...
+}
